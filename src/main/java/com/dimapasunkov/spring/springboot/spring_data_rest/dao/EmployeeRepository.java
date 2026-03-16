56c85b814325db7d84551a80672105a103588ad4
Dimitri Pasunkov <javarusher01@gmail.com>
2026-03-16 16:42:40 +0100

    Initial commit

---
+++ src/main/java/com/dimapasunkov/spring/springboot/spring_data_rest/dao/EmployeeRepository.java
@@ -0,0 +1,8 @@
+package com.dimapasunkov.spring.springboot.spring_data_rest.dao;
+
+import com.dimapasunkov.spring.springboot.spring_data_rest.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Integer > {
+
+}
